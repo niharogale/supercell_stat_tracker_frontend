c77b5d6bc62d1ca0c65bdceb816449dceefc7725
Joseph Sun <jossun@umich.edu>
2025-08-14 17:00:44 -0400

    Establish clash royale page

--- supercell_stat_tracker_frontend/app/clashRoyale/page.tsx
+++ supercell_stat_tracker_frontend/app/clashRoyale/page.tsx
@@ -1,11 +1,11 @@
 import Link from "next/link";
 
-export default function Home() {
+export default function clash() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <h1>Hello World</h1>
-      <h2>BITCH</h2>
-      <Link href="/clashRoyale">Dashboard</Link>
+      <h2>BITCH HO</h2>
+      <Link href="/">Home</Link>
     </div>
 
   );
